57cd3abd26ec1d79ffcb8af385624d973e2aaa42
paged.csci <paged.csci@45bcece9-43c0-49af-4449-131fea715c50>
2011-11-03 20:33:47 +0000

    Page -- 	Login Page 	Edits in ~/login/login.php

--- hgwebsite/etagamma/login/login.php
+++ hgwebsite/etagamma/login/login.php
@@ -9,35 +9,37 @@ if(isset($_POST['roll'])){
 	$rowcount = mysql_num_rows($result);
 	if($rowcount==1){
 		$_SESSION['user']=$roll;
-echo <<< END
-Login Successful!<br>
-<a href = "/#">Homepage</a>
-<script type="text/javascript">
-function redirect(){
- window.location="/#";
-}
-setTimeout('redirect()',1000);
-</script>
+		echo <<< END
+			Login Successful!<br>
+			<a href = "/#">Homepage</a>
+			<script type="text/javascript">
+			function redirect(){
+				window.location="/#";
+			}
+			setTimeout('redirect()',1000);
+		</script>
 END;
-	} else {
+	} else if($rowcount==0) {
 		echo 'Login Failed!';
 		if(strtoupper($password)==$password){
 			echo "<br><br>Was caps lock on?";
 		}
 		die();
+	} else {
+		echo 'fuck you ben!';
 	}
 	
 } else {
-echo <<< END
-<center>
-<form method = "POST" action=""><table><tr>
-<td><b>Roll Number</b></td>
-<td><input type="text" name = "roll" size = "15"></td>
-<tr><td><b>Password</b></td><td><input type="password" name = "pass" size = "15"></td>
-</tr><tr><td><input type = "submit" value = "Login"></td>
-</tr></table></form>
+	echo <<< END
+	<center>
+	<form method = "POST" action=""><table><tr>
+	<td><b>Roll Number</b></td>
+	<td><input type="text" name = "roll" size = "15"></td>
+	<tr><td><b>Password</b></td><td><input type="password" name = "pass" size = "15"></td>
+	</tr><tr><td><input type = "submit" value = "Login"></td>
+	</tr></table></form>
 
-<br><a href="forgotpass.php">Forgot your password?</a></center>
+	<br><a href="forgotpass.php">Forgot your password?</a></center>
 
 END;
 }?>
